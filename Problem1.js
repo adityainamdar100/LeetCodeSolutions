4781dac28a49f800ebf4925682eb7c58d87d6838
adityainamdar100 <40563668+adityainamdar100@users.noreply.github.com>
2020-05-05 17:06:49 +0530

    Subarray Sum Equals K - LeetCode Medium

---
+++ Problem1.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    let cnt = 0;
+    for(let i=0;i<nums.length;i++) {
+        let sum = 0;
+        for(let j=i;j<nums.length;j++) {
+            sum += nums[j];
+            sum === k ? cnt+=1 : '';
+        }
+    }
+    return cnt;
+};
